645e572aecfe3c22fa021796d3e4fdd64726bad4
abel <abelignacioalarcon4@gmail.com>
2026-03-03 09:57:08 -0500

    asistencia

--- resources/views/pages/asistenciaest/pdfasistencia.blade.php
+++ resources/views/pages/asistenciaest/pdfasistencia.blade.php
@@ -15,7 +15,7 @@
                   <div>
                      <span>Escoger Aula:</span>
                      <div>
-                        @forelse($turno as $tu)
+                        @forelse($aula as $tu)
                          <div class="form-check">
                              <input class="form-check-input" type="radio" name="turno" id="grado" value="{{$tu->id}}" style="cursor:pointer">
                              <label class="form-check-label" for="estado">
